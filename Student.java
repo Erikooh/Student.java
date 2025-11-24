521ef9033d259100d00c735a642b572c5974fa4e
Erikooh <erickwanyama1564@gmail.com>
2025-11-24 23:33:18 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,17 +3,15 @@
 // Date: Nov 24, 2025
 
 public class Student {
-    // Attributes
+
     String name;
     double marks;
-
-    // Constructor
+    
     public Student(String name, double marks) {
         this.name = name;
         this.marks = marks;
     }
 
-    // Method to display student details
     public void displayDetails() {
         System.out.println("Student Name: " + name + ", Marks: " + marks);
     }
